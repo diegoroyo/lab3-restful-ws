2d1ba59e737d0da1120f7754c2bca72449a3d565
diegoroyo <diego.roymen@gmail.com>
2019-11-14 13:44:27 +0100

    Implemented equals for domain classes and copy constructor

--- src/main/java/rest/addressbook/domain/PhoneNumber.java
+++ src/main/java/rest/addressbook/domain/PhoneNumber.java
@@ -24,4 +24,20 @@ public class PhoneNumber {
     this.type = type;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    // self check
+    if (this == o)
+        return true;
+    // null check
+    if (o == null)
+        return false;
+    // type check and cast
+    if (getClass() != o.getClass())
+        return false;
+    PhoneNumber other = (PhoneNumber) o;
+    return ((this.number == null && other.number == null)
+            || (this.number != null && this.number.equals(other.number))) &&
+           this.type.equals(other.type);
+  }
 }
